9ec366361c0c370e2fb0a23ef2b0bdee5da15679
Anika Christopher <anika.mchristopher@gmail.com>
2019-10-13 17:46:52 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["red", "yellow", "blue", "green"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+ def add_element_to_end_of_array =  ["red", "yellow", "blue", "green"] 
+   add_element_to_end_of_array << "white"
+ end 
+
+def add_element_to_start_of_array(array, element) = ["red", "yellow", "blue", "green"]
+  add_element_to_end_of_array.unshift("purple")
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def remove_element_from_end_of_array(array) = ["red", "yellow", "blue", "green"]
+  remove_element_from_end_of_array.pop 
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_start_of_array = ["red", "yellow", "blue", "green"]
+  remove_element_from_start_of_array.shift   
 end
 
-def remove_element_from_start_of_array(array)
-  
+def retrieve_element_from_index = ["red", "yellow", "blue", "green"]
+  retrieve_element_from_index[2]
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_first_element_from_array = ["red", "yellow", "blue", "green"]
+  retrieve_first_element_from_array[0]
 end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array = ["red", "yellow", "blue", "green"]
+  retrieve_last_element_from_array[-1]
 end
